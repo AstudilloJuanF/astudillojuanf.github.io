e5287a975ab20a91bd45da36bca0cca62e945807
AstudilloJuanF <chito288@gmail.com>
2021-05-09 22:45:45 -0400

    Update homepage

--- scripts/main.js
+++ scripts/main.js
@@ -17,111 +17,6 @@ window.addEventListener('load', responsivePageFooter);
 window.addEventListener('resize', responsivePageFooter);
 window.addEventListener('deviceorientation', responsivePageFooter);
 
-//--------------------------------------------------------------------------------
-
-
-
-// AJAX project snippet cards
-
-const projectSection = document.getElementById('projects-section');
-
-function requestProjectCard(url, projecType){
-
-	var doc, meta, cardImgSrc, cardVideoSrc, cardTitle, cardDescription, cardProjectType = projecType;
-
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function(){
-		if (xhr.status === 200 && xhr.readyState === 4) {
-
-			doc = xhr.response; // doc = new DOMParser().parseFromString(xhr.response, 'text/html');
-			meta = doc.head.getElementsByTagName('meta');
-
-			for (var i = 0; i < meta.length; i++){
-				if (meta[i].hasAttribute('property')){
-					meta[i].attributes.property.value === 'og:title' && !cardTitle ? cardTitle = meta[i].content : undefined;
-					meta[i].attributes.property.value === 'og:description' && !cardDescription ? cardDescription = meta[i].content : undefined;
-					meta[i].attributes.property.value === 'og:image' && !cardImgSrc ? cardImgSrc = meta[i].content : undefined;
-					meta[i].attributes.property.value === 'og:video' && !cardVideoSrc ? cardVideoSrc = meta[i].content : undefined;
-				}
-			}
-
-			if (!(!cardTitle && !cardDescription && !cardImgSrc)){
-
-				if (typeof cardVideoSrc != 'undefined')  {
-					var cardvideoHTMLTemplate = `<video class="project-card-video" src="${cardVideoSrc}" type="video/mp4" muted loop></video>`;
-				} else {
-					var cardvideoHTMLTemplate = '';
-				}
-
-
-				var cardHTMLTemplate = `<div class="project-card" style="opacity: 0; transform: translateY(-20px);">
-	<a class="project-link" href="${url}">
-		<h4 class="project-card-title">${cardTitle}</h4>
-		<figure class="project-card-media-wrap">
-			<img class="project-card-img" src="${cardImgSrc}" alt="${cardTitle}">
-			${cardvideoHTMLTemplate}
-		</figure>
-		<div class="pj-card-description-box">
-			<p class="pj-card-description">${cardDescription}</p>
-		</div>
-		<p class="project-type">${cardProjectType}</p>
-	</a>
-</div>`;
-
-				projectSection.insertAdjacentHTML('beforeend', cardHTMLTemplate);
-				
-				var currentCard = document.getElementsByClassName('project-card');
-				currentCard = currentCard[currentCard.length-1];
-
-				setTimeout(function(){
-					currentCard.setAttribute('style', 'opacity: 1; transform: none;');
-					currentCard.removeAttribute('style');
-				}, 1000);
-			}
-
-			resizeCards();
-			responsivePageFooter();
-
-			var projectCards = projectSection.getElementsByClassName('project-card');
-
-			for(var i = 0; i < projectCards.length; i++){
-
-				projectCards[i].setAttribute('data-id', i);
-				projectCards[i].removeEventListener('pointerenter', toggleCardVideo);
-				projectCards[i].addEventListener('pointerenter', toggleCardVideo);
-				
-			}
-
-		} else {
-			//handle error
-		}
-	};
-
-	xhr.responseType = 'document';
-	xhr.open('GET', url);
-	xhr.send();
-}
-
-requestProjectCard('boosterlander/', '2D Minigame');
-requestProjectCard('sharerbox/', 'Frontent Development');
-
-function resizeCards(){
-	var cardImgs = document.getElementsByClassName('project-card-media-wrap');
-
-	for (var i = 0; i < cardImgs.length; i++){
-
-		if (document.body.offsetWidth <= 450){
-			cardImgs[i].style.width = `${document.body.offsetWidth * 0.925}px`;
-			cardImgs[i].style.height = `${cardImgs[i].offsetWidth * (52.25 / 100)}px`;
-		} else {
-			cardImgs[i].removeAttribute('style');
-		}
-	}
-}
-
-window.addEventListener('load', resizeCards);
-window.addEventListener('resize', resizeCards);
-window.addEventListener('deviceorientation', resizeCards);
 
 // -----------------------------------------------------------------------------------
 
@@ -161,23 +56,38 @@ for(var i = 0; i < languageSelect.childElementCount; i++){
 	languageSelect.children[i].value === currentLanguage ? languageSelect.children[i].selected = true : languageSelect.children[i].selected = false;	
 }
 
-var xhrLanguage = new XMLHttpRequest();
-
 var languages, languageJSON;
 
-xhrLanguage.onreadystatechange = function(){
-	if(xhrLanguage.status === 200 && xhrLanguage.readyState === 4){
-		languages = JSON.parse(xhrLanguage.response);
+var xhrLanguage = new XMLHttpRequest();
 
-		languageJSON = languages[currentLanguage];
+var languagePromise = new Promise((resolve, reject)=>{
+	xhrLanguage.onreadystatechange = function(){
 
-		updateLanguage();
+		if(xhrLanguage.status === 200 && xhrLanguage.readyState === 4){
+			languages = JSON.parse(xhrLanguage.response);
+	
+			languageJSON = languages[currentLanguage];
+	
+			resolve('languages loaded');
+	
+		}else if(xhrLanguage.status.toString().match(/404|403|500/)){
+			reject('not found');
+		}
+	};
+	
+	xhrLanguage.open('GET', 'languages/supported-languages.json');
+	xhrLanguage.send();
+});
 
-	}
-
-};
-xhrLanguage.open('GET', 'languages/supported-languages.json');
-xhrLanguage.send();
+languagePromise.then((fulfilled)=>{
+	requestProjectCard('boosterlander/', `${languageJSON.minigame} (2D)`, 'boosterlander, 2D minigame');
+	requestProjectCard('sharerbox/', `${languageJSON.development} (${languageJSON.frontend})`, 'sharerbox, frontend development');
+	updateLanguage();
+})
+.catch((rejected)=>{
+	requestProjectCard('boosterlander/', '2D Minigame', '2D minigame');
+	requestProjectCard('sharerbox/', 'Frontend Development', 'frontend development');
+});
 
 function updateLanguage(){
 
@@ -202,13 +112,36 @@ function updateLanguage(){
 			languageSelect.children[i].selected ? flagImg[i].style.display = 'block' : flagImg[i].removeAttribute('style');
 		}
 
-		professionTitle.innerText = `${languageJSON.webDeveloper} ${languageJSON.hyphen} ${languageJSON.gameDeveloper}`;
+		professionTitle.innerHTML = `<code>&lt;<span class="monokai-pink">${languageJSON.webDeveloper}</span>/&gt;<br><span class="monokai-green">&</span><br><span class="monokai-purple">${languageJSON.gameDeveloper}<span class="pointer">&VerticalSeparator;</span></code>`;
 		projectsTitle.innerText = languageJSON.projects;
 
 		footerColumnsTitles[0].innerText = languageJSON.pages;
 		footerColumnsTitles[1].innerText = languageJSON.projects;
 		footerColumnsTitles[2].innerText = languageJSON.contact;
 		footerColumnsTitles[2].nextElementSibling.firstElementChild.title = languageJSON.email;
+	}
+
+	var projectCards = projectSection.getElementsByClassName('project-card');
+
+	if(typeof projectCards !== 'undefined'){
+		for(var i = 0; i < projectCards.length; i++){
+
+			var tags = projectCards[i].getAttribute('data-tags').split(',');
+			console.log(projectCards[i].getAttribute('data-tags').split(','));
+			var typeElement = projectCards[i].getElementsByClassName('project-type')[0];
+
+			for(var u = 0; u < tags.length; u++){
+
+				tags[u] = tags[u].trim();
+
+				var typeText;
+
+				tags[u].match(/boosterlander/i) ? typeText = `${languageJSON.minigame} (2D)` : undefined;
+				tags[u].match(/sharerbox/i) ? typeText = `${languageJSON.development} (${languageJSON.frontend})` : undefined;
+
+				typeElement.innerText = typeText;
+			}
+		}
 	}
 }
 
@@ -226,8 +159,113 @@ window.addEventListener('scroll', function(){
 	projectsTitle.style.boxShadow = shadowVal;
 });
 
+//--------------------------------------------------------------------------------
+
+
+// AJAX project snippet cards
+
+const projectSection = document.getElementById('projects-section');
+
+function requestProjectCard(url, projecType, projectTags){
+
+	var doc, meta, cardImgSrc, cardVideoSrc, cardTitle, cardDescription; 
+	var cardProjectType = projecType, cardProjectTags = projectTags;
+
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		if (xhr.status === 200 && xhr.readyState === 4) {
+
+			doc = xhr.response; // doc = new DOMParser().parseFromString(xhr.response, 'text/html');
+			meta = doc.head.getElementsByTagName('meta');
+
+			for (var i = 0; i < meta.length; i++){
+				if (meta[i].hasAttribute('property')){
+					meta[i].attributes.property.value === 'og:title' && !cardTitle ? cardTitle = meta[i].content : undefined;
+					meta[i].attributes.property.value === 'og:description' && !cardDescription ? cardDescription = meta[i].content : undefined;
+					meta[i].attributes.property.value === 'og:image' && !cardImgSrc ? cardImgSrc = meta[i].content : undefined;
+					meta[i].attributes.property.value === 'og:video' && !cardVideoSrc ? cardVideoSrc = meta[i].content : undefined;
+				}
+			}
+
+			if (!(!cardTitle && !cardDescription && !cardImgSrc)){
+
+				if (typeof cardVideoSrc != 'undefined')  {
+					var cardvideoHTMLTemplate = `<video class="project-card-video" src="${cardVideoSrc}" type="video/mp4" muted loop></video>`;
+				} else {
+					var cardvideoHTMLTemplate = '';
+				}
+
+
+				var cardHTMLTemplate = `<div class="project-card" style="opacity: 0; transform: translateY(-20px);" data-tags="${cardProjectTags}">
+	<a class="project-link" href="${url}">
+		<h4 class="project-card-title">${cardTitle}</h4>
+		<figure class="project-card-media-wrap">
+			<img class="project-card-img" src="${cardImgSrc}" alt="${cardTitle}">
+			${cardvideoHTMLTemplate}
+		</figure>
+		<div class="pj-card-description-box">
+			<p class="pj-card-description">${cardDescription}</p>
+		</div>
+		<p class="project-type">${cardProjectType}</p>
+	</a>
+</div>`;
+
+				projectSection.insertAdjacentHTML('beforeend', cardHTMLTemplate);
+				
+				var currentCard = document.getElementsByClassName('project-card');
+				currentCard = currentCard[currentCard.length-1];
+
+				setTimeout(function(){
+					currentCard.setAttribute('style', 'opacity: 1; transform: none;');
+					currentCard.removeAttribute('style');
+				}, 1000);
+			}
+
+			resizeCards();
+			responsivePageFooter();
+
+			var projectCards = projectSection.getElementsByClassName('project-card');
+
+			for(var i = 0; i < projectCards.length; i++){
+
+				projectCards[i].setAttribute('data-id', i);
+				
+				projectCards[i].removeEventListener('pointerenter', toggleCardVideo);
+				projectCards[i].addEventListener('pointerenter', toggleCardVideo);
+			}
+
+		} else {
+			//handle error
+		}
+	};
+
+	xhr.responseType = 'document';
+	xhr.open('GET', url);
+	xhr.send();
+}
+
+function resizeCards(){
+	var cardImgs = document.getElementsByClassName('project-card-media-wrap');
+
+	for (var i = 0; i < cardImgs.length; i++){
+
+		if (document.body.offsetWidth <= 450){
+			cardImgs[i].style.width = `${document.body.offsetWidth * 0.925}px`;
+			cardImgs[i].style.height = `${cardImgs[i].offsetWidth * (52.25 / 100)}px`;
+		} else {
+			cardImgs[i].removeAttribute('style');
+		}
+	}
+}
+
+window.addEventListener('load', resizeCards);
+window.addEventListener('resize', resizeCards);
+window.addEventListener('deviceorientation', resizeCards);
+
+
 
 // ----------------------------------------------------------------------------------
+
 
 					/* Video playback */
 
